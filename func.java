9c0869be099cc3cc63f5f32498f9a7bbb3e4244c
unknown <h20170252@pilani.bits-pilani.ac.in>
2019-07-29 13:12:38 +0530

    Initial commit-1

---
+++ func.java
@@ -0,0 +1,29 @@
+
+public class func {
+
+	public int add(int x,int y)
+	{
+		return x+y;
+	}
+	public int sub(int x,int y)
+	{
+		return x-y;
+	}
+	public int mul(int x,int y)
+	{
+		return x*y;
+	}
+	public int div(int x,int y)
+	{
+		return x/y;
+	}
+public static void main()
+{
+	
+	func f= new func();
+	f.add(3,2);
+	f.sub(2,3);
+	f.mul(2,3);
+	f.div(2,3);
+}
+}
